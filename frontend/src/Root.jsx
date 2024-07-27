ab2c319c11617a74089141f17411f65c71164300
PRATIK PRABHAKAR <pratik.prabhakar17@gmail.com>
2024-07-28 00:05:40 +0530

    Modules completed with MULTI - Language Videos

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,13 +1,14 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Nav/Navbar";
 import Hero from "./components/Hero/Hero";
+import Modules from "./components/Modules";
 
 const Root = () => {
   return (
     <div>
       <Navbar />
-
-      <Outlet />
+      <Modules></Modules>
+      <Outlet />  
     </div>
   );
 };
