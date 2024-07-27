ceb43b0437a479759eb7837ee55e52531af7e8fb
Shreyas M N <95998181+shreyas-m-n@users.noreply.github.com>
2024-07-27 23:33:43 +0530

    Update Register.jsx
    added role

--- frontend/components/Register/Register.jsx
+++ frontend/components/Register/Register.jsx
@@ -5,8 +5,6 @@ import axios from "axios";
 import "./Register.css";
 
 function Register() {
-  const [file, setFile] = useState(null);
-  const [res, setRes] = useState(null);
   const [user, setUser] = useState({
     name: "",
     email: "",
@@ -34,8 +32,14 @@ function Register() {
       for (const key in user) {
         formData.append(key, user[key]);
       }
-      await axios.post("http://localhost:3000/api/v1/users/register", formData);
-      console.log("User created successfully");
+      const response = await axios.post("http://127.0.0.1:5000/register", formData, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        withCredentials: true,
+      });
+      console.log(user, "from frontend")
+      console.log("User created successfully", response.data);
     } catch (err) {
       console.log(err);
     }
@@ -53,155 +57,145 @@ function Register() {
   return (
     <div className="register">
       <div className="cont-reg">
-        <div className="nav-main-reg">
-          <div className="opt-reg">
-            <span className="red"></span>
-            <span className="yellow"></span>
-            <span className="green"></span>
+        <form onSubmit={handleSubmit} className="register-form">
+          <div className="form-header">
+            <FaUser className="icon-reg" />
+            <h1>Create New Account</h1>
           </div>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="leftform">
-            <div className="line-reg">
-              <FaUser className="icon-reg" /> <h1>Create New Account</h1>
-            </div>
 
-            <label htmlFor="name">Name</label>
-            <input
-              name="name"
-              type="text"
-              placeholder="John Doe"
-              onChange={handleChange}
-            />
-            <label htmlFor="email">Email</label>
-            <input
-              name="email"
-              type="email"
-              placeholder="email@example.com"
-              onChange={handleChange}
-            />
-            <label htmlFor="password">Password</label>
-            <input
-              name="password"
-              type="password"
-              placeholder=""
-              onChange={handleChange}
-            />
-            <label htmlFor="age">Age</label>
-            <input
-              name="age"
-              type="number"
-              placeholder="30"
-              onChange={handleChange}
-            />
-            <label htmlFor="contact">Phone Number</label>
-            <input
-              name="contact"
-              type="text"
-              placeholder="+1 234 567 89"
-              onChange={handleChange}
-            />
+          <label htmlFor="name">Name</label>
+          <input
+            name="name"
+            type="text"
+            placeholder="John Doe"
+            onChange={handleChange}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            type="email"
+            placeholder="email@example.com"
+            onChange={handleChange}
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            name="password"
+            type="password"
+            placeholder="password"
+            onChange={handleChange}
+          />
+          <label htmlFor = "role">Role</label>
+          <input
+            name = "role" 
+            type = "text"
+            placeholder="Role"
+            onChange={handleChange}
+          />
+            
+          <label htmlFor="age">Age</label>
+          <input
+            name="age"
+            type="number"
+            placeholder="30"
+            onChange={handleChange}
+          />
+          <label htmlFor="contact">Phone Number</label>
+          <input
+            name="contact"
+            type="text"
+            placeholder="+1 234 567 89"
+            onChange={handleChange}
+          />
+          <label htmlFor="gender">Gender</label>
+          <div className="gender-options">
             <div>
-              <label>Gender</label>
-              <div style={{ display: 'flex', marginTop: '10px' }}>
-                <div style={{ marginRight: '20px' }}>
-                  <input
-                    type="radio"
-                    name="gender"
-                    id="male"
-                    value="Male"
-                    onChange={handleChange}
-                  />
-                  <label htmlFor="male">Male</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    name="gender"
-                    id="female"
-                    value="Female"
-                    onChange={handleChange}
-                  />
-                  <label htmlFor="female">Female</label>
-                </div>
-              </div>
+              <input
+                type="radio"
+                name="gender"
+                id="male"
+                value="Male"
+                onChange={handleChange}
+              />
+              <label htmlFor="male">Male</label>
             </div>
-
-            <motion.button
-              variants={buttonVariants}
-              whileHover="hover"
-              whileTap="tap"
-              type="submit"
-            >
-              Register
-            </motion.button>
-          </div>
-          <div className="rightform">
-            <div className="line-reg">
-              <FaPenNib className="icon-reg" /> <h1>Basic Information</h1>
-            </div>
-
-            <label htmlFor="area">Area</label>
-            <input
-              name="area"
-              type="text"
-              placeholder="Bangalore"
-              onChange={handleChange}
-            />
-            <label htmlFor="shg">SHG</label>
-            <input
-              name="shg"
-              type="text"
-              placeholder="SHG Name"
-              onChange={handleChange}
-            />
-            <label htmlFor="married">Married</label>
-            <input
-              name="married"
-              type="text"
-              placeholder="Married/Single"
-              onChange={handleChange}
-            />
-            <label htmlFor="income">Income</label>
-            <input
-              name="income"
-              type="number"
-              placeholder="1000"
-              onChange={handleChange}
-            />
-            <label htmlFor="loans">Loans</label>
-            <input
-              name="loans"
-              type="number"
-              placeholder="1000"
-              onChange={handleChange}
-            />
             <div>
-              <label>Assets</label>
-              <div style={{ display: 'flex', marginTop: '10px' }}>
-                <div style={{ marginRight: '20px' }}>
-                  <input
-                    type="radio"
-                    name="assets"
-                    id="yes"
-                    value="Yes"
-                    onChange={handleChange}
-                  />
-                  <label htmlFor="yes">Yes</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    name="assets"
-                    id="no"
-                    value="No"
-                    onChange={handleChange}
-                  />
-                  <label htmlFor="no">No</label>
-                </div>
-              </div>
+              <input
+                type="radio"
+                name="gender"
+                id="female"
+                value="Female"
+                onChange={handleChange}
+              />
+              <label htmlFor="female">Female</label>
             </div>
           </div>
+          <label htmlFor="area">Area</label>
+          <input
+            name="area"
+            type="text"
+            placeholder="Bangalore"
+            onChange={handleChange}
+          />
+          <label htmlFor="shg">SHG</label>
+          <input
+            name="shg"
+            type="text"
+            placeholder="SHG Name"
+            onChange={handleChange}
+          />
+          <label htmlFor="married">Married</label>
+          <input
+            name="married"
+            type="text"
+            placeholder="Married/Single"
+            onChange={handleChange}
+          />
+          <label htmlFor="income">Income</label>
+          <input
+            name="income"
+            type="number"
+            placeholder="1000"
+            onChange={handleChange}
+          />
+          <label htmlFor="loans">Loans</label>
+          <input
+            name="loans"
+            type="number"
+            placeholder="1000"
+            onChange={handleChange}
+          />
+          <label htmlFor="assets">Assets</label>
+          <div className="assets-options">
+            <div>
+              <input
+                type="radio"
+                name="assets"
+                id="yes"
+                value="Yes"
+                onChange={handleChange}
+              />
+              <label htmlFor="yes">Yes</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                name="assets"
+                id="no"
+                value="No"
+                onChange={handleChange}
+              />
+              <label htmlFor="no">No</label>
+            </div>
+          </div>
+          <motion.button
+            variants={buttonVariants}
+            whileHover="hover"
+            whileTap="tap"
+            type="submit"
+            className="register-button"
+          >
+            Register
+          </motion.button>
         </form>
       </div>
     </div>
